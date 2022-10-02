4c815c376d4e3a373de9dfa556a2104ad42887d8
Bogdan Vlad <averagevlad@gmail.com>
2022-10-02 15:47:48 +0100

    Add custom useEffect hook

---
+++ src/hooks/useDidMountEffect.tsx
@@ -0,0 +1,35 @@
+import { useEffect, EffectCallback, DependencyList, useRef } from "react";
+
+/**
+ * This hook gets called only when the dependencies change but not during initial render.
+ *
+ * @param {EffectCallback} effect The `useEffect` callback function.
+ * @param {DependencyList} deps An array of dependencies.
+ *
+ * @example
+ * ```
+ *  useDidMountEffect(()=>{
+ *      alert("Dependency changed!");
+ * },[dependency]);
+ * ```
+ */
+export const useDidMountEffect = (
+	effect: EffectCallback,
+	deps?: DependencyList
+) => {
+	const initialRender = useRef(true);
+
+	useEffect(() => {
+		let effectReturns: void | (() => void) = () => {};
+
+		if (initialRender.current) {
+			initialRender.current = false;
+		} else {
+			effectReturns = effect();
+		}
+
+		if (effectReturns && typeof effectReturns === "function") {
+			return effectReturns;
+		}
+	}, deps);
+};
